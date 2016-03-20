c98b4dffa31a3668a74afe4ba45c070139f6b116
Travis DePuy <tdepuy@gmail.com>
2016-03-19 23:53:39 -0700

    Cleaned up logging. Added callback to send_once.

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,26 @@ var inspect = require( 'util' ).inspect;
 router.post( '/irsend', function( req, res, next ) {
 	var lirc = req.app.locals.lirc;
 
-  console.log( 'Req: ' + inspect( req.query, null, 3 ) );
-  console.log( 'Remote: ' + req.query.remote + '. Cmd: ' + req.query.code );
+  // console.log( 'Req: ' + inspect( req.query, null, 3 ) );
+  // console.log( 'Remote: ' + req.query.remote + '. Cmd: ' + req.query.code );
 
+  // We should probably sanitize the inputs here. 
+  // Unless we can get lirc_node to use something other than exec
   lirc.irsend.send_once( req.query.remote, req.query.code, function ( err, stdout, stderr ) {
+
   	if( err ) {
-  		console.log( 'exec error: ' + err );
+  		console.log( 'Error running send_once: ' + err );
+  		res.status( 500 ).send( 'It broke' );
+
   	}
 
-  	console.log( 'stdout: ' + stdout );
+  	res.status( 200 ).send( 'OK' );
+
   } );
   
-
-  res.status( 200 ).send( 'OK' );
+  // It's probably ok. We won't really know 
+  // because of the async nature of this stuff
+  //res.status( 200 ).send( 'OK' );
 });
 
 module.exports = router;
